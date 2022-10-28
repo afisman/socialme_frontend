a048fe33ea946577817085faae18087b0dbc2ed5
afisman <afisman@gmail.com>
2022-10-28 18:12:01 +0200

    created fetchUser util, finished with Pin component, changed logo

--- src/components/Pin.jsx
+++ src/components/Pin.jsx
@@ -7,12 +7,46 @@ import { BsFillArrowUpCircleFill } from 'react-icons/bs';
 
 
 import { client, urlFor } from '../client';
+import { fetchUser } from '../utils/fetchUser';
 
-const Pin = ({ pin: { postedBy, image, _id, destination } }) => {
+const Pin = ({ pin: { postedBy, image, _id, destination, save } }) => {
     const [postHovered, setPostHovered] = useState(false);
     const [savingPost, setSavingPost] = useState(false);
-
     const navigate = useNavigate();
+    const user = fetchUser();
+    const alreadySaved = !!(save?.filter((item) => item.postedBy._id === user.googleId))?.length;
+
+    const savePin = (id) => {
+        if (!alreadySaved) {
+            setSavingPost(true);
+
+            client
+                .patch(id)
+                .setIfMissing({ save: [] })
+                .insert('after', 'save[-1]', [{
+                    _key: uuidv4(),
+                    userId: user?.googleId,
+                    postedBy: {
+                        _type: 'postedBy',
+                        _ref: user?.googleId,
+                    },
+                }])
+                .commit()
+                .then(() => {
+                    window.location.reload();
+                    setSavingPost(false);
+                });
+        }
+    }
+
+    const deletePin = (id) => {
+        client
+            .delete(id)
+            .then(() => {
+                window.location.reload();
+            })
+    }
+
 
     return (
         <div className='m-2'>
@@ -34,16 +68,76 @@ const Pin = ({ pin: { postedBy, image, _id, destination } }) => {
                                     href={`${image?.asset?.url}?dl=`}
                                     download
                                     onClick={(e) => e.stopPropagation()}
-                                    className='bg-white w-9 h-9 rounded-full flex items-center justify-center text-dark tex-xl opacity-75'
+                                    className='bg-white w-9 h-9 rounded-full flex items-center justify-center text-dark tex-xl opacity-75 hover:opacity-100 hover:shadow-md outline-none'
                                 >
                                     <MdDownloadForOffline />
                                 </a>
                             </div>
-
+                            {alreadySaved ? (
+                                <button
+                                    type='button'
+                                    className='bg-red-500 opacity-70 hover:opacity-100 text-white font-bold px-5 py-1 text-base rounded-3xl hover:shadow-md outlined-none'
+                                    onClick={(e) => {
+                                        e.stopPropagation();
+                                    }}
+                                >
+                                    {save?.length} Saved
+                                </button>
+                            ) :
+                                (<button
+                                    onClick={(e) => {
+                                        e.stopPropagation();
+                                        savePin(_id);
+                                    }}
+                                    type='button'
+                                    className='bg-red-500 opacity-70 hover:opacity-100 text-white font-bold px-5 py-1 text-base rounded-3xl hover:shadow-md outlined-none'
+                                >
+                                    Save
+                                </button>
+                                )}
+                        </div>
+                        <div className='flex justify-between items-center gap-2 w-full'>
+                            {destination && (
+                                <a
+                                    href={destination}
+                                    target='_blank'
+                                    rel='noreferrer'
+                                    className='bg-white flex items-center gap-2 text-black font-bold p-2 pl-4 pr-4 rounded-full opacity-70 hover:opacity-100 hover:shadow-md'
+                                    onClick={(e) => {
+                                        e.stopPropagation();
+                                    }}
+                                >
+                                    <BsFillArrowUpCircleFill />
+                                    {destination.length > 20 ?
+                                        destination.slice(8, 28)
+                                        :
+                                        destination.slice(8)}
+                                </a>
+                            )}
+                            {postedBy?._id === user.googleId && (
+                                <button
+                                    onClick={(e) => {
+                                        e.stopPropagation();
+                                        deletePin(_id);
+                                    }}
+                                    type='button'
+                                    className='bg-white p-2 opacity-70 hover:opacity-100 font-bold text-dark text-base rounded-3xl hover:shadow-md outlined-none'
+                                >
+                                    <AiTwotoneDelete />
+                                </button>
+                            )}
                         </div>
                     </div>
                 )}
             </div>
+            <Link to={`user-profile/${postedBy?._id}`} className='flex gap-2 mt-2 items-center' >
+                <img
+                    src={postedBy?.image}
+                    alt="user-profile"
+                    className='w-8 h-8 rounded-full object-cover'
+                />
+                <p className='font-semibold capitalize'>{postedBy?.userName}</p>
+            </Link>
         </div>
     )
 }
